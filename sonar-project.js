27a3ef89651a93564cafb9de5c1ff11f2bfff87f
mfrimponm <115571635+mfrimponm@users.noreply.github.com>
2023-01-10 19:42:19 -0700

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '3e9ee4f64a40359b3f9c904bc34392412e2c9d80',
+	    'sonar.login': 'ee80c14e4af0e77ff5018acfd755bd5ce529cca8',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
